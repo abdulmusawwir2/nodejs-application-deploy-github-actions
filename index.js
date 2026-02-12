22595217e7e653bb3a8bc6c5528958d22b160f73
Abdul Musawwir <abdulmusawwir611@gmail.com>
2026-02-12 22:24:37 +0530

    new commit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+    console.log("This is newwer version after deployment with CI/CD pipeline");
     console.log(`http://localhost:3000`);
 });
 
